9ba84615b08d36c8afd355e283af34915700838c
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-06-11 17:25:19 +0900

    feat: add clock component

---
+++ client/src/components/contents/main/clock.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+const Clock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const formattedTimeParts = time
+    .toLocaleTimeString("en-US", { hour12: false })
+    .split(":");
+
+  const formattedTime = `${formattedTimeParts[0]}:${formattedTimeParts[1]} ${formattedTimeParts[2]}`;
+
+  return (
+    <>
+      <p className="text-4xl font-bold mt-20">
+        Meeting Minutes 에 오신 것을 환영합니다!
+      </p>
+      <p
+        className="mt-20 text-6xl font-bold"
+        style={{ fontFamily: "Tiny5", textShadow: "2px 2px 2px #000000" }}
+      >
+        {formattedTime}
+      </p>
+    </>
+  );
+};
+
+export default Clock;
--- client/src/components/contents/main/main.js
+++ client/src/components/contents/main/main.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Clock from "./clock";
 
 const Main = () => {
+  
+
   return (
     <div className="flex flex-col w-4/5 h-screen">
-      <div className="flex justify-center items-center h-full">
-        <h1 className="text-4xl font-bold">
-          Welcome to the Meeting Minutes App
-        </h1>
+      <div className="text-center items-center h-full">
+        <Clock />
       </div>
     </div>
   );
