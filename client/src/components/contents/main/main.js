ba2faf735978c86157b459bcd3b7c1cd1b283831
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-06-12 16:55:20 +0900

    feat: sidebar animation

--- client/src/components/contents/main/main.js
+++ client/src/components/contents/main/main.js
@@ -8,7 +8,7 @@ const Main = () => {
   const isCollapsed = useSelector(state => state.isCollapsed);
 
   return (
-    <div className={`flex flex-col w-full h-screen ${isCollapsed ? "pl-40" : "pl-64"}`}>
+    <div className={`flex flex-col w-full h-screen transition-all duration-75 ease-in-out ${isCollapsed ? "pl-40" : "pl-64"}`}>
       <div className="text-center items-center h-full">
         <Clock />
         <RecentlyView />
--- client/src/components/contents/my_minutes/myMinutes.js
+++ client/src/components/contents/my_minutes/myMinutes.js
@@ -5,7 +5,7 @@ const MyMinutes = () => {
   const isCollapsed = useSelector(state => state.isCollapsed);
 
   return (
-    <div className={`flex flex-col w-full ${isCollapsed ? "pl-40" : "pl-64"} h-screen`}>
+    <div className={`flex flex-col w-full transition-all duration-75 ease-in-out ${isCollapsed ? "pl-40" : "pl-64"} h-screen`}>
       <div className="flex justify-center items-center h-full">
         <h1 className="text-4xl font-bold">My Minutes</h1>
       </div>
