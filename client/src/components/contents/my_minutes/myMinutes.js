398239516c477f0381af3da5363e8572b7f2310c
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-06-13 19:47:55 +0900

    feat: add myMinutes component

--- client/src/components/contents/my_minutes/myMinutes.js
+++ client/src/components/contents/my_minutes/myMinutes.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from 'react-redux';
+import apiClient from "api";
 
 const MyMinutes = () => {
+  const [allMinutes, setAllMinutes] = useState([]);
+
   const isCollapsed = useSelector(state => state.isCollapsed);
 
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
+    apiClient.get(`/minutes/user/${user.id}`).then((response) => {
+      setAllMinutes(response.data);
+      console.log(response.data);
+    });
+  }, [user.id]);
+
   return (
-    <div className={`flex flex-col w-full transition-all duration-75 ease-in-out ${isCollapsed ? "pl-40" : "pl-64"} h-screen`}>
-      <div className="flex justify-center items-center h-full">
-        <h1 className="text-4xl font-bold">My Minutes</h1>
+    <div className="bg-yellow-50 w-full h-screen">
+      <div className={`flex flex-col w-full transition-all duration-75 ease-in-out ${isCollapsed ? "pl-40" : "pl-64"}`}>
+        <div className="mt-20 ml-20">
+          <p className="font-bold text-2xl">{user.name}님의 회의록</p>
+        </div>
+        <div className="flex flex-wrap w-full">
+          {allMinutes.map((minutes, index) => (
+            <div key={index} className="w-1/2 p-16">
+              <div className="border-2 border-gray-300 bg-white rounded-lg p-5">
+                <p className="font-bold text-xl">{minutes.title}</p>
+                <p className="text-lg">{minutes.content}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
